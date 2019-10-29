1058ee15f682046ff1a8b15136371dad80d1f3f1
bilal <aslan89@outlook.com>
2019-10-29 15:47:45 +0300

    - ikinci versiyon

--- odoo_benart_modified/models/parameter.py
+++ odoo_benart_modified/models/parameter.py
@@ -9,8 +9,12 @@ class Parameter(models.Model):
     _rec_name = 'paramter_value'
 
     parameter_name = fields.Selection([('certification_body', 'Certification Body'), ('accreditation', 'Accreditation'),
+                                       ('document_attachment', 'Document'),
                                        ('document_type', 'Document Type')],
                                       string='Parameter Name')
 
     paramter_value = fields.Char('Value')
     is_active = fields.Boolean('Is Active?', default=True)
+
+    file = fields.Binary('File')
+    file_name = fields.Char('File Name')
