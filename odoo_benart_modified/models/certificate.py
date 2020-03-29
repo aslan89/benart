bb04d2c3a8c2f6f820bc695576fd278ecd398be5
aslan89 <aslan89@outlook.com>
2020-03-29 20:58:12 +0300

    CANLIYA GEÇİŞ ÖNCESİ SON TALEPLER YAPILDI

--- odoo_benart_modified/models/certificate.py
+++ odoo_benart_modified/models/certificate.py
@@ -11,8 +11,8 @@ class Certificate(models.Model):
     _rec_name = 'certification_number'
     _inherit = ['mail.thread', 'mail.activity.mixin']
 
-    # certification_body_id = fields.Many2one('benart.parameter',
-    #                                         domain="[('parameter_name', '=', 'certification_body'),('is_active', '=', True)]")
+    certification_body_id = fields.Many2one('benart.parameter',
+                                            domain="[('parameter_name', '=', 'certification_body'),('is_active', '=', True)]")
     accreditation_id = fields.Many2one('benart.parameter', translate=True, track_visibility="onchange",
                                        domain="[('parameter_name', '=', 'accreditation'),('is_active', '=', True)]")
     document_type_id = fields.Many2one('benart.parameter', translate=True, track_visibility="onchange",
@@ -20,7 +20,7 @@ class Certificate(models.Model):
                                        required=True)
     res_partner_id = fields.Many2one('res.partner', required=True, string="Firm", translate=True,
                                      track_visibility="onchange")
-    # advicer_id = fields.Many2one('benart.advicer', string="Advicer")
+    advicer_id = fields.Many2one('benart.advicer', string="Advicer")
     scope = fields.Text("Scope", required=True, translate=True, track_visibility="onchange")
     certification_number = fields.Char("Certification Number", required=True, track_visibility="onchange")
     standart_definition = fields.Char("Standart Definition", required=True, track_visibility="onchange")
